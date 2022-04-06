52acb245593104149190aa0c2bc4a7a0a56a4519
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-06 12:41:45 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,24 +8,9 @@ node 'slave1.puppet', 'slave2.puppet' {
   ensure => running,
   enable => true,
  }
-}
-
-
-node 'slave1.puppet' {
+  
  file {'/var/www/html/index.html':
-  ensure => file,
-  source => 'puppet:///modules/static/index.html'
- }
-}
-
-node 'slave2.puppet' {
-
- package { 'php':
-  ensure => latest,
- }
-
- file {'/var/www/html/index.php':
-  ensure => file,
-  source => 'puppet:///modules/dynamic/index.php'
+ ensure => file,
+ source => 'puppet:///modules/static/index.html'
  }
 }
