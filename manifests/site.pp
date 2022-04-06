098fc2d1e834b2ce0a59204b62bb14ee9fa60ff5
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-06 12:17:26 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,3 +11,21 @@ node 'slave1.puppet', 'slave2.puppet' {
 }
 
 
+node slave1.puppet {
+ file {'/var/www/php/index.html':
+  ensure => file,
+  source => 'puppet:///modules/static/index.html'
+ }
+}
+
+node slave2.puppet {
+
+ package { 'php':
+  ensure => latest,
+ }
+
+ file {'/var/www/php/index.php':
+  ensure => file,
+  source => 'puppet:///modules/dynamic/index.php'
+ }
+}
