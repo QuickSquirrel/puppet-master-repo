eebab3b51dea4a6b89f44fd0a1d9afd26c51fed2
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-06 15:04:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,17 +14,23 @@ node 'slave1.puppet', 'slave2.puppet' {
  enable => false,
  }
 
- 
- file { '/var/www/html/index.html':
+ file {'/var/www/html/index.html':
  ensure => file,
- content => '<html>
-  <head>
-    <title>Success!</title>
-  </head>
-  <body>
-    You Vagrantfile is fine if you can see this message.
-  </body>
-</html>',
+ source => 'master.puppet:///files/static/index.html'
  }
+
+
+ 
+# file { '/var/www/html/index.html':
+# ensure => file,
+# content => '<html>
+#  <head>
+#    <title>Success!</title>
+#  </head>
+#  <body>
+#    You Vagrantfile is fine if you can see this message.
+#  </body>
+#</html>',
+# }
  
 }
